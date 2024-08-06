239052d5e13e1587d8c38b4267c5c8ff73f5c430
Liuisei <116147262+Liuisei@users.noreply.github.com>
2024-08-06 12:16:26 +0900

    Update InGameManage Add SoundManager

---
+++ GameJamJupiter/GameJamJupiter/Assets/Ryuu/SoundManager.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+[DefaultExecutionOrder(-100)]
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance = null;
+
+
+    [SerializeField] AudioMixer _audioMixer;
+    [SerializeField] AudioSource _bgmAudioSource;
+    [SerializeField] AudioSource _seAudioSource;
+
+    protected void Awake()
+    {
+        if (_bgmAudioSource == null) Debug.LogError("BGM AudioSource is null.");
+        if (_seAudioSource == null) Debug.LogError("SE AudioSource is null.");
+        if (_audioMixer == null) Debug.LogError("Audio Mixer is null.");
+
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    /// <summary> BGMを再生 </summary> ///
+    public void PlayBGM(AudioClip audioClipBGM)
+    {
+        _bgmAudioSource.clip = audioClipBGM;
+        _bgmAudioSource.Play();
+    }
+
+    /// <summary> SEを再生 </summary> ///
+    public void PlaySE(AudioClip audioClipSE)
+    {
+        _seAudioSource.PlayOneShot(audioClipSE);
+    }
+
+    public void MuteBGM(bool mute)
+    {
+        _bgmAudioSource.mute = mute;
+    }
+
+    public void MuteSE(bool mute)
+    {
+        _seAudioSource.mute = mute;
+    }
+
+    public void SetVolumeBGM(float volume)
+    {
+        _audioMixer.SetFloat("BGM", volume);
+    }
+    
+    public void SetVolumeSE(float volume)
+    {
+        _audioMixer.SetFloat("SE", volume);
+    }
+}
