011549327ee198957ee39b4f52c8ad9056cba083
Liuisei <116147262+Liuisei@users.noreply.github.com>
2024-08-06 13:48:59 +0900

    Action

--- GameJamJupiter/GameJamJupiter/Assets/Ryuu/InGameManager.cs
+++ GameJamJupiter/GameJamJupiter/Assets/Ryuu/InGameManager.cs
@@ -1,5 +1,7 @@
 using System;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
+
 /// <summary>
 /// ゲームシーンに居るクラスです インゲームを管理します
 /// IngameMAnager.Instance で呼び出して下さい
@@ -8,34 +10,48 @@ using UnityEngine;
 public class InGameManager : MonoBehaviour
 {
     public static InGameManager Instance = null;
-    
+
     private int _characterID = 0;
-    private int _itemID = 0;
+    [SerializeField] private int _itemID = 0;
     private int _rodID = 0;
     private InGameState _state;
+
+    public InGameState State
+    {
+        get => _state;
+        set
+        {
+            _state = value;
+            OnStateChanged?.Invoke(_state);
+        }
+    }
+
     private GameObject _item;
     private GameObject _camera;
-    
-    
+
+
     public Action<InGameState> OnStateChanged;
-    
-    
+
+
     
 
 
     
-    
+    public void ZoomedIn()
+    {
+        _camera.transform.position = new Vector3(0, 0, -10);
+    }
     //public void ATK(enum enum float)
-    
     void Awake()
     {
         Instance = this;
     }
-    
 }
 public enum InGameState
 {
     Start,
     Fishing,
-    Release,
+    ReleaseUP,
+    ReleaseDawn,
+    ReleaseEnd
 }
