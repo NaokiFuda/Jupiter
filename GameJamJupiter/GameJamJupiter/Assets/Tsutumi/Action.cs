1ae1bec09bbbb18bdc7b81bfd59005f57db616f2
mikenchi <mikenti337564@gmail.com>
2024-08-06 12:56:03 +0900

    [fix]Angleのフラグの修正

--- GameJamJupiter/GameJamJupiter/Assets/Tsutumi/Action.cs
+++ GameJamJupiter/GameJamJupiter/Assets/Tsutumi/Action.cs
@@ -4,32 +4,93 @@ using UnityEngine;
 
 public class Action : MonoBehaviour
 {
-    [SerializeField] string _charge;
-    [SerializeField] string _release;
-    float  _time;
-    bool _timeRec = false;  
-      
-    
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    [SerializeField] string _chargeKey;
+    [SerializeField] string _releaseKey;
+    [SerializeField] string _highKey;
+    [SerializeField] string _midKey;
+    [SerializeField] string _lowKey;
+    [SerializeField] float[] _resultPaturn;
+    Result _result;
+    Angle _angle;
+    float _time;
+    float _chargeTime;
+    float _power;
+    bool _angleSet = true;
+    bool _timeRec;
+    bool _end;
     void Update()
     {
+        if (_angleSet)
+        {
+            _angle = Set();
+        }
         if (_timeRec)
         {
             _time += Time.deltaTime;
         }
-        if (Input.GetButton(_charge))
+        if (_angleSet == false &&Input.GetButton(_chargeKey))
         {
-
+            _chargeTime += Time.deltaTime;
         }
-        if (Input.GetButtonUp(_charge))
+        else if (_angleSet == false && Input.GetButtonUp(_chargeKey))
         {
+            float _power = PowerCount(_chargeTime);
+            _timeRec = true;
+        }
+        else if (_angleSet == false && _timeRec && Input.GetButtonDown(_releaseKey))
+        {
+            _timeRec = false;
+            _end = true;
+        }
 
+        if (_end)
+        {
+            for(int i = 0; i < _resultPaturn.Length; i++)
+            {
+             if(_time > _resultPaturn[i])
+                {
+                    continue;
+                }
+                _result = (Result)i;
+                break;
+            }
+            (_result, _angle, _power);
         }
     }
-   
+    Angle Set()
+    {
+        if (Input.GetButtonDown(_highKey))
+        {
+            _angleSet = false;
+            return Angle.high;
+        }
+        else if (Input.GetButtonDown(_midKey))
+        {
+            _angleSet = false;
+            return Angle.mid;  
+        }
+        else if (Input.GetButtonDown(_lowKey))
+        {
+            _angleSet = false;
+            return Angle.low;
+        }
+        return Angle.low;
+    }
+    float PowerCount(float power)
+    {
+        float pow = CharacterManager.power * power;
+        return pow;
+    }
+}
+public enum Angle
+{
+    high,
+    mid,
+    low
+}
+public enum Result
+{
+    ultimate,
+    great,
+    faild
 }
